b9267043c4f8588081c72a85293af203ee39ba92
Luis Gonzalo Guzman Sanchez <105834318+GUSL-Luis-Gonzalo@users.noreply.github.com>
2022-05-22 14:34:03 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # prog21
-programa para un simulador de ahorro.
+<p><b>Autor:</b> Luis Gonzalo Guzman Sanchez, <b>Fecha:</b> 03/03/2022 <br>
+  <b>Escuela:</b> Universidad del Valle de Mexico <b>Campus</b> Villahermosa <br>
+  <b>Materia:</b> Programación Estructurada <br>
+  <b>Ciclo:</b> 01/2022</p>
+<br>
+<p>Este es un programa de  funciones y ciclo en Lenguaje C de la materia de Programación Estructurada<br>
+Muestra el uso de:
+  <ul>
+    <li>Variables flotantes y enteros</li>
+    <li>scanf</li>
+    <li>for</li>
+    <li>El uso de include para las librerias</li>
+    <li>Comentarios para la documentación interna del programa</li>
+    </ul>
+    </p>
+<br>
+<b>Objetivo:</b> Hacer un programa para un simulador de ahorro.
+<br>
+<br>
+<p><b>Breve descripción:</b>
+Es un programa que le muestra al usuario cuanto dinero obtendrá si ahorra
+una cantidad en un número de meses en su sucursal, el banco da actualmente %4 mensual.
+<br>
+Hacer una función que reciba como parámetro, la cantidad de dinero a ahorrar y a cuántos meses, 
+ la función retornará el total de dinero ahorrado más intereses.
+<br>
+</p>
---
+++ prog21.cpp
@@ -0,0 +1,42 @@
+/* Autor: Luis Gonzalo Guzman Sanchez, Realizado: 03/03/2022
+Escuela: Universidad del Valle de Mexico Campus Villahermosa 
+Materia: Programación Estructurada 
+Ciclo: 01/2022
+
+Este es un programa de  funciones y ciclo en Lenguaje C de la materia de Programación Estructurada
+Muestra el uso de:
+  
+    -Variables flotantes y enteros
+    printf para mostrar mensajes y variables
+    -scanf
+    -for
+    -El uso de include para las librerias
+    -Comentarios para la documentación interna del programa
+*/
+#include<stdio.h>
+//prototipos
+float calcularAhorro(float capital, int meses);
+//Main
+int main() {
+	//Declaración de variable
+	float dinero, ahorro;
+	int periodo;
+	//Proceso
+	printf("Introduce la cantidad a ahorrar: $");
+	scanf("%f",&dinero);
+	printf("Indica a cuantos meses invertiras tu dinero: ");
+	scanf("%d",&periodo);
+	ahorro=calcularAhorro(dinero,periodo);
+	//Salida
+	printf("Al final del periodo obtendras. %.2f", ahorro);
+	return 0;
+}
+//Función
+float calcularAhorro(float capital, int meses){
+	//Declaración
+	float total=capital;
+	for(int i=0;i<meses;i++){ //Ciclo
+		total= total*0.04+total;
+	}
+	return total;
+}
